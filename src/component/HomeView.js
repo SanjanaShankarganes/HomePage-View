854e5c422ca18d70d1dafe456e69b1675e1d677d
sabarish-10 <sabarishcse2023@ksrce.ac.in>
2024-08-26 12:40:42 +0530

    feat:changed styles

--- src/component/HomeView.js
+++ src/component/HomeView.js
@@ -1,58 +1,49 @@
 import React from 'react';
-
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import card1Image from '../asserts/List-Picture.png';
 import card2Image from '../asserts/Map-Picture.png';
 import card3Image from '../asserts/Reg-Picture.png';
+import Footer from './Footer';
 const HomeView = () => {
   const handleCardClick = (cardNumber) => {
     alert(`Card ${cardNumber} clicked`);
   };
   return (
-    <div className="container">
-      <div
-        className="card"
-        onClick={() => handleCardClick(1)}   
-      >
-       
-        <div className="card-body">
-          <h2 className="card-title">View List</h2>
-        </div>
-        <img
-          src={card1Image} 
-          alt="Card 1"
-        />
-        <p className="card-content">Entire list of idols and applicants</p>
+    
+          <div class="card-div row d-flex justify-content-evenly">
+          <div class="card col-sm-12 col-md-4 col-lg-3 my-4 mx-3" onClick="handleCardClick(1)">
+          <span class="card-body">
+                  <span class="h3 card-title">View List</span>
+              </span>
+                  <img  src={card1Image} 
+          alt="Card 1" />
+                  <p class="card-content mt-5">Entire list of idols and applicants</p>
+              
+          </div>
+          <div class="card col-sm-12 col-md-4 col-lg-3 my-4 mx-3" onClick="handleCardClick(2)">
+              <span class="card-body">
+                  <span class="h3 card-title">Map</span>
+              </span>
+              <img  src={card2Image} 
+          alt="Card 1" />
+              <p class="card-content mt-5">Location of idols and tracking</p>
+          </div>
+          <div class="card col-sm-12 col-md-4 col-lg-3 my-4 mx-3 " onClick="handleCardClick(3)">
+              <div class="card-body text-start">
+                  <p class="card-title h3">Registration Count</p>
+              </div>
+              <img  src={card3Image} 
+           alt="Card 3" />
+              <div class="card-content mt-5">
+                  <p>Total number of idols registered:</p>
+                  <p>Number of idols immersed:</p>
+                  <p>Number of idols not immersed:</p>
+              </div>
+          </div>
+          <Footer />
       </div>
-      <div
-        className="card"
-        onClick={() => handleCardClick(2)}
-      >
-        
-        <div className="card-body">
-          <h2 className="card-title">Map</h2>
-         
-        </div>
-        <img
-          src={card2Image}  
-          alt="Card 2"
-        />
-         <p className="card-content">Location of idols and tracking</p>
-      </div>
-      <div
-        
-      >
-        <div className="card-body">
-          <h2 className="card-title">Registration Count</h2>
-        </div>
-        <img
-          src={card3Image}  
-          alt="Card 3"
-        />
-        <p >Total number of idols registered:  </p>
-          <p >Numbered of idols immersed :  </p>
-          <p >Number of idols not immersed:  </p>
-      </div>
-    </div>
+      
   );
 };
 
